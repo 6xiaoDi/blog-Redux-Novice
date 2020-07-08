42ec11f80d9fa22832939d65a4c3a96ab51e8c12
Mark Liu <ldtheended@163.com>
2020-07-08 14:11:02 +0800

    v0.06-4-example06-4（把同步dispatch 变成异步 dispatch 的中间件，打开包装函数，传递异步函数，报错）

--- app/src/components/Item.js
+++ app/src/components/Item.js
@@ -30,28 +30,22 @@ class Item extends React.Component {
         //     }
         // });
         // 这个函数只能是同步的
-        function updateAction() {
-            // return {
-            //     type: 'UPDATE_ITEMS',
-            //     payload: {
-            //         items: []
-            //     }
-            // }
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    resolve({
-                        type: 'UPDATE_ITEMS',
-                        payload: {
-                            items: []
-                        }
-                    });
-                }, 1000);
-            })
+        // async function updateAction(dispatch) {
+        async function updateAction() {
+            let rs = await axios({
+                url: '/api/items'
+            });
+
+
+            return {
+                type: 'UPDATE_ITEMS',
+                payload: {
+                    items: []
+                }
+            }
         }
 
         // dispath 最终是接受一个 action 对象，我们可以通过函数来产生这样的一个对象，但是这个函数必须是同步的
-
-        // dispatch 会被修改，这里的 dispatch 并不是真正的原来的 dispatch 函数，
         this.props.dispatch( updateAction() );
     }
 
